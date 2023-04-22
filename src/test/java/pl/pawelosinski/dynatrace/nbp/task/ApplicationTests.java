1e3a34abfd99f526b95a49b30138e0c8724c4321
Paweł Osiński <osik2000@wp.pl>
2023-04-22 19:51:05 +0200

    Initial commit

---
+++ src/test/java/pl/pawelosinski/dynatrace/nbp/task/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pawelosinski.dynatrace.nbp.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
